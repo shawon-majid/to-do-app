3655408419af23495e4bb3b4ce80b5305646049b
shawon-majid <shawon.majid@gmail.com>
2023-07-23 12:22:59 +0600

    refactored home.js

--- public/home.js
+++ public/home.js
@@ -1,9 +1,7 @@
 
 
 const taskList = document.querySelector('.list-of-task');
-
 const form = document.forms['To-Do-Form'];
-
 const logoutButton = document.querySelector('.logout-button');
 
 
@@ -21,108 +19,97 @@ addTaskHtml = (id, taskName) => {
 
     task.innerHTML = html;
     taskList.appendChild(task)
-
-
 }
 
 
+// get all tasks
+
 fetch('/tasks').then((response) => {
     response.json().then((data) => {
-
         data.forEach(element => {
-
             console.log(element.taskId, element.taskName);
-
             addTaskHtml(element.taskId, element.taskName);
         });
-
     })
 })
 
+
+// submit button
 form.addEventListener('submit', e => {
     e.preventDefault(); // prevents from restarting
 
     var taskName = document.getElementById('task-name').value;
-
     if (taskName) {
-
         // will generate some id
         var id = generateRandomId(8);
-
-
         addTaskHtml(id, taskName);
 
         // now add this to database
-
         const data = {
             taskId: `${id}`,
             taskName: taskName,
         }
 
-        fetch('/tasks', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data),
-        }).then((val) => {
-            console.log('Added Successfully')
-        })
-
+        addTask(data);
 
         document.getElementById('task-name').value = "";
     }
 
 })
 
-
-
-
-clickDeleteButton = (button, id) => {
-    console.log('delete button clicked')
-
-    console.log(button.parentElement.parentElement);
-
-    const liElement = button.parentElement.parentElement; // button's parent = div, div's parent = li
-
-    taskList.removeChild(liElement);
-
-    // then remove it from the database;
-
-    fetch(`/tasks/${id}`, {
-        method: 'DELETE',
-    }).then((val) => {
-        console.log('Deleted Successfully')
-    });
-}
-
-clickEditButton = (id) => {
-    console.log('Edit button clicked')
-
-    window.location.href = `/edit/${id}`;
-    // taskList.parent
-}
-
-
-
+// logoutButton
 logoutButton.addEventListener('click', async () => {
     // Perform logout functionality here
-
     const response = await fetch('/logout');
-
     if (response.ok) {
         window.location.href = '/';
     }
     else {
         alert("logout request failed. Try again!");
     }
-
 });
 
 
 
+// buttons
 
-function generateRandomId(length) {
+clickDeleteButton = (button, id) => {
+    const liElement = button.parentElement.parentElement; // button's parent = div, div's parent = li
+    taskList.removeChild(liElement);
+    removeTask(id);
+}
+
+
+clickEditButton = (id) => {
+    console.log('Edit button clicked')
+    window.location.href = `/edit/${id}`;
+}
+
+
+
+
+// api calls
+
+addTask = (data) => {
+    fetch('/tasks', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data),
+    })
+}
+
+
+removeTask = (id) => {
+    fetch(`/tasks/${id}`, {
+        method: 'DELETE',
+    })
+}
+
+
+// miscelinoous
+generateRandomId = (length) => {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
 
